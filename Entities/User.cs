de6e8887724b893b630ef833d3cdfd98007d3071
DarkArmy <luis_munoz@ucne.edu.do>
2019-09-01 13:29:14 -0400

    Initial Commit

---
+++ Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id_User { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(int id_User, string name, string lastName, string userName, string password, DateTime createdAt)
+        {
+            Id_User = id_User;
+            Name = name;
+            LastName = lastName;
+            UserName = userName;
+            Password = password;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ ProyectoFinalA2.Tests/UserUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BLL;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProyectoFinalA2.Tests
+{
+    [TestClass]
+    public class UserUnitTest
+    {
+        [TestMethod]
+        public void Save()
+        {
+            BaseRepository<User> br = new BaseRepository<User>();
+            bool ok = br.Save(GetUser());
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            BaseRepository<User> br = new BaseRepository<User>();
+            User user = br.Get(1);
+            Assert.IsTrue(user != null);
+        }
+
+        [TestMethod]
+        public void GetList()
+        {
+            BaseRepository<User> br = new BaseRepository<User>();
+            bool ok = br.GetList(x => true).Count > 0;
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            BaseRepository<User> br = new BaseRepository<User>();
+            bool ok = br.Delete(1);
+            Assert.IsTrue(ok);
+        }
+
+        #region AuxiliaryMethods
+        private User GetUser()
+        {
+            User user = new User();
+            user.Name = "Luis";
+            user.LastName = "Apell";
+            user.UserName = "lluis";
+            user.Password = "123456";
+            user.CreatedAt = DateTime.Now;
+            return user;
+        }
+        #endregion
+    }
+}
